5053089a578ed97013d02e1f71f0d53d73f39cce
Sean Mee <sean@MacBook-Pro-2.lan1>
2020-12-10 11:52:31 -0800

    validate email for event endpoint

--- util/events/newUserSignup.js
+++ util/events/newUserSignup.js
@@ -1,17 +1,24 @@
 const { iterableUrl } = require('../../config/keys');
+const emailValidation = require('../validateEmail');
 
 module.exports = function(req, res, axios) {
-    const event_data = {
-        "email": req.body.email,
-        "eventName": "newUserSignup",
-        "dataFields": {
-            "signupSource": "ProWeb LP"
-        }
-    };
-    axios.post(iterableUrl+'events/track', event_data)
-        .then(response => {
-            res.send(response.data)
-        }).catch(err => {
+    const email = req.body.email;
+    const {hasErrors, message} = emailValidation(email);
+    if(hasErrors) {
+        res.send(message);
+    }else{
+        const event_data = {
+            "email": email,
+            "eventName": "newUserSignup",
+            "dataFields": {
+                "signupSource": req.body.signUpSource ? req.body.signUpSource : "Unknown Signup Source"
+            }
+        };
+        axios.post(iterableUrl+'events/track', event_data)
+            .then(response => {
+                res.send(response.data)
+            }).catch(err => {
             res.send(err)
         })
+    }
 }
